6ae2600e5cfe1644575df3bab9cac7e07b403c93
viglinat <prokhorov109@gmail.com>
2020-08-02 00:27:46 +0300

    v0.0.1 working with openweathermap API making request of current weather

--- openweather.py
+++ openweather.py
@@ -5,19 +5,20 @@ from datetime import datetime
 #conversion from unix timestamp to readable date-time:
 #print(datetime.utcfromtimestamp(dt).strftime('%Y-%m-%d %H:%M:%S'))
 
-APIkey = 'ee3c9674a0567d97347a5b1a34e8171f'
-city = 'Sevastopol'
+def main():
+    APIkey = 'ee3c9674a0567d97347a5b1a34e8171f'
+    city = 'Sevastopol'
 
-weather = ['weather', 'main', 'wind', 'clouds']
+    #r = api.get_current_weather(city, APIkey)
+    r = api.get_forecast(city, APIkey)
+    print('message cod: ', r.json()['cod'])
 
-r = api.get_current_weather(city, APIkey)
+    with open('log.txt', 'w') as log:
+        log.write(r.text)
 
-with open('log.txt', 'w') as log:
-    log.write(r.text)
+    data = r.json()
 
-data = r.json()
+    api.print_formatted_weather(data)
 
-print('current GMT+3 time : ', datetime.utcfromtimestamp(data['dt']).strftime('%Y-%m-%d %H:%M:%S'))
-for param in weather:
-     if param in data:
-         print(param, ' : ', data[param])
+if (__name__ == '__main__'):
+    main()
--- openweathermapapi.py
+++ openweathermapapi.py
@@ -1,11 +1,51 @@
 import requests
+messages = {200 : 'OK', 401 : 'invalid API key'}
+
+def get_error_message(code_message):
+    return messages[code_message]
 
 def get_current_weather(city, APIkey):
-    current_weather_path = 'https://api.openweathermap.org/data/2.5/weather'
+    #api request for current weather
+    api_addres = 'https://api.openweathermap.org/data/2.5/weather'
     APIparams = {'q' : city, 'APPID' : APIkey}
-    request_handler = requests.get(current_weather_path, params = APIparams)
+    request_handler = requests.get(api_addres, params = APIparams)
     return request_handler
 
-def get_5_day_forecast(city, APIkey):
-    current_weather_path = 'api.openweathermap.org/data/2.5/forecast'
+def get_forecast(city, APIkey):
+    #api request for 5 day / 3 hour forecast
+    api_addres = 'https://api.openweathermap.org/data/2.5/forecast'
     APIparams = {'q' : city, 'APPID' : APIkey}
+    request_handler = requests.get(api_addres, params = APIparams)
+    #forecast returns list of dictionaries
+    #weather['list'][1]['main']['temp'] example
+    return request_handler
+
+def print_formatted_weather(weather):
+    #weather is dictionary about weather data
+    #prints formatted output for 5 day forecast or current weather
+    if 'list' in weather:
+        #if there is a "list" param, weather is a forecast,
+        #else, weather is a current weather
+        counter = weather['cnt']
+        weather = weather['list']
+    else:
+        counter = -1
+
+    if counter >= 0:
+        i = 0
+        prev_day = weather[0]['dt_txt'][8:10] #positions 8,9 contains month's day
+        while i < counter:
+            cur_day = weather[i]['dt_txt'][8:10]
+            if (cur_day != prev_day):
+                print('date: ', weather[i]['dt_txt'][0:11])
+            print('UTC time: ',weather[i]['dt_txt'][11:19], '\ntemperature: ', weather[i]['main']['temp'] - 273.15)
+            print('clouds: ', weather[i]['weather'][0]['main'],\
+                ' ', weather[i]['clouds']['all'])
+            prev_day = cur_day
+            i = i + 1
+            print('\n')
+    else:
+        print('date & time: ', weather['dt_txt'])
+        print('temperature: ', weather['main']['temp'] - 273.15)
+        print('clouds: ', weather['weather']['main'],
+            ' ', weather['clouds']['all'])
